46b0985b410b796e56a4a66b3b06588ce2eea51f
Keshav Gupta <keshav.gupta@seaflux.tech>
2024-01-10 18:08:55 +0530

    feat: Background Colour is added

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            StyledText("Roll The Dice!"),
+          body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Colors.blue,
+            Colors.blueGrey,
           ],
+        )),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              StyledText("Roll The Dice!"),
+            ],
+          ),
         ),
       )),
     ),
   );
 }
+
+// import 'package:flutter/material.dart';
+
+// class ContainerDecoration extends StatelessWidget {
+//   @override
+//   Widget build(context) {
+//     return;
+//   }
+// }
